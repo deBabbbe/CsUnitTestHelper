a78f885f7ae841a35c6ac0eaf2dbae64f5c76f2f
deBabbbe <neotbh@gmx.de>
2022-12-21 14:34:21 +0000

    Add additional test fixes

--- TestHelperTest/TempCreateFileTest.cs
+++ TestHelperTest/TempCreateFileTest.cs
@@ -12,10 +12,11 @@ public class TempCreateFileTests
         const string path = "%temp%/Temp.txt";
         FileAssert.DoesNotExist(path.ExpandEnv());
         var expectedText = Guid.NewGuid().ToString();
+        using(new TempSetEnvVar("temp", "."))
         using (new TempCreateFile(path, expectedText))
         {
             FileAssert.Exists(path.ExpandEnv(), expectedText);
-            Assert.AreEqual(expectedText, File.ReadAllText(path));
+            Assert.AreEqual(expectedText, File.ReadAllText(path.ExpandEnv()));
         }
         FileAssert.DoesNotExist(path.ExpandEnv());
     }
