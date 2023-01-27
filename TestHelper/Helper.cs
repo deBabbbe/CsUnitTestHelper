f11dd88bb4d4a918d8f0780612f859670a1a1063
deBabbbe <neotbh@gmx.de>
2023-01-27 22:46:20 +0100

    Add small refactoring

--- TestHelper/Helper.cs
+++ TestHelper/Helper.cs
@@ -45,11 +45,9 @@ public static class Helper
     public static bool HasPropertyWithAttribute<T>(this T _, string propertyName, Type attribute) where T : class
     {
         var property = typeof(T).GetProperty(propertyName);
-        if (property == null)
-        {
-            return false;
-        }
-        return Attribute.IsDefined(property, attribute);
+        return property == null
+            ? false
+            : Attribute.IsDefined(property, attribute);
     }
 
     private static string ConvertCharToRandomCase(char charText) =>
